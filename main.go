c5957a3956764d7d360e8342e559a3fea394faef
paoloumali <2522851+paoloumali@users.noreply.github.com>
2023-08-31 15:20:28 +0800

    add log middleware

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Logger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// now time
+		t := time.Now()
+		// handle the request
+		c.Next()
+
+		latency := time.Since(t)
+		status := c.Writer.Status()
+		// log latency and status  too
+		log.Printf("latency: %s, status: %d", latency, status)
+	}
+}
 
 func main() {
 	r := gin.Default()
+	r.Use(Logger())
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
